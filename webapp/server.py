df3ffbaebf8906236cba657f7e9b89a102a0bb27
Roma637 <74816460+Roma637@users.noreply.github.com>
2023-04-07 19:35:30 +0800

    Update server.py

--- webapp/server.py
+++ webapp/server.py
@@ -5,46 +5,53 @@ import os
 
 app = Flask(__name__)
 
+#these are global variables, so define them here
 response = []
 filepath = ''
 rhyme = ''
 
+#the home page
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
-    
+    return render_template('index.html') #the initial form
+
+#this is when you are presented with 5 possible options
 @app.route("/choices", methods=['GET', 'POST'])
 def options():
+    #import these two
     global response
     global rhyme
+    
     rhyme = request.form['nursery_rhyme']
     topic = request.form['topic']
     keywords = request.form['keywords'].split(",")
+    
+    #making the actual request to gpt
     response = ask_gpt(rhyme, topic, keywords)
 
+    #intermediate.html renders the list of 5 options
     return render_template('intermediate.html', response=response, seq=[i for i in range(len(response))])
     
+#the last page to visit
 @app.route("/audio", methods=['GET', 'POST'])
 def audio():
+    #import these
     global response
     global filepath
     global rhyme
-    num = request.form["which_one"] # which option of the 5 song variants
+    
+    # which option of the 5 song variants
+    num = request.form["which_one"] 
     final_rhyme = response[int(num)] 
-    # print(final_rhyme)
 
     filepath = generate_music(final_rhyme, rhyme)
-    # nfilepath = os.path.join('..', filepath)
-    # print('filepath:')
-    # print(nfilepath)
-
 
     return render_template('download.html', final_rhyme=final_rhyme)
 
+#actually serves the file to the user
 @app.route("/download", methods=['GET', 'POST'])
 def serve():
     global filepath
-
     return send_file(filepath, as_attachment=True)
 
 if __name__=="__main__":
